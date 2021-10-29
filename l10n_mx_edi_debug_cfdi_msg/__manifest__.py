d2950b465f0b1ef547436051c518b597f7f04e38
Germán Ponce Dominguez <german.ponce@argil.mx>
2021-10-29 15:03:30 -0500

    Modulo Debug

---
+++ l10n_mx_edi_debug_cfdi_msg/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# Coded by German Ponce Dominguez 
+#     ▬▬▬▬▬.◙.▬▬▬▬▬  
+#       ▂▄▄▓▄▄▂  
+#    ◢◤█▀▀████▄▄▄▄▄▄ ◢◤  
+#    █▄ █ █▄ ███▀▀▀▀▀▀▀ ╬  
+#    ◥ █████ ◤  
+#     ══╩══╩═  
+#       ╬═╬  
+#       ╬═╬ Dream big and start with something small!!!  
+#       ╬═╬  
+#       ╬═╬ You can do it!  
+#       ╬═╬   Let's go...
+#    ☻/ ╬═╬   
+#   /▌  ╬═╬   
+#   / \
+# Cherman Seingalt - german.ponce@outlook.com
+{
+    "name"      : "Mensajes Debug CFDI Previo",
+    "version"   : "14.0",
+    "author"    : "German Ponce Dominguez",
+    "website"   : "https://www.fixdoo.mx",
+    "license"   : "OEEL-1",
+    "category"  : "Localization/Mexico",
+    "description": """
+
+""",
+    
+    "depends": [
+        "hr",
+        "l10n_mx_edi",
+        ],
+    "data"    : [
+    ],
+        "installable": True,
+}
